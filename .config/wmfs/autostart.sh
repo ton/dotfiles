3eb4c573b8f5fd00c94c9b2758a4bd986dae6935
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2011-06-20 10:41:25 +0200

    Tweaks to wmfs configuration.

--- .config/wmfs/autostart.sh
+++ .config/wmfs/autostart.sh
@@ -2,7 +2,7 @@
 
 # Pick a random wallpaper from Dropbox.
 wallpapers=(~/Dropbox/Photos/Wallpapers/*)
-display -window root ${wallpapers[$[$RANDOM % ${#wallpapers[@]}]]}
+display -window root -crop 1920x1080+0+0 ${wallpapers[$[$RANDOM % ${#wallpapers[@]}]]}
 
 # Open four terminals on tag 2 and 3.
 if [ `pidof urxvtd | wc -l` -lt 1 ]
