651fb2c1aebfd30446e1b2a6d5bc17a9b518052e
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2017-09-14 16:00:22 +0200

    Add initial simple installation script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+ln -sfr .dircolors $HOME/
+ln -sfr .inputrc $HOME/
+ln -sfr .gtkrc-2.0 $HOME/
+
+ln -sfr .gitconfig $HOME/
+ln -sfr .gitignore $HOME/
+
+ln -sfr .i3 $HOME/
+ln -sfr .i3blocks.conf $HOME/
+
+ln -sfr .Xdefaults $HOME/
+ln -sfr .xinitrc $HOME/
+
+ln -sfr .zshrc $HOME/
+ln -sfr .zshenv $HOME/
+ln -sfr .zsh $HOME/
+ln -sfr .zprofile $HOME/
+
+ln -sfr .tmux.conf $HOME/
+ln -sfr .tmux.make.conf $HOME/
