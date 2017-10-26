5c5f6b46ba8d052329f8e8649188232c89b7e55b
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2017-10-26 22:44:17 +0200

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+ln -sfr .dircolors $HOME/
+ln -sfr .inputrc $HOME/
+ln -sfr .gtkrc-2.0 $HOME/
+
+ln -sfr .gitconfig $HOME/
+ln -sfr .gitignore $HOME/
+
+ln -sfr .i3 $HOME/
+ln -sfr .i3blocks.conf $HOME/
+
+ln -sfr .Xdefaults $HOME/
+ln -sfr .xinitrc $HOME/
+
+ln -sfr .zshrc $HOME/
+ln -sfr .zshenv $HOME/
+ln -sfr .zsh $HOME/
+ln -sfr .zprofile $HOME/
+
+ln -sfr .tmux.conf $HOME/
+ln -sfr .tmux.make.conf $HOME/
+
+ln -sfr ccache.conf $HOME/
+
+ln -sfr .config/compton.$(hostname).conf $HOME/.config/
+ln -sfr .config/fontconfig $HOME/.config/
+ln -sfr .config/cmus $HOME/.config/
+ln -sfr .config/nvim $HOME/.config/
+
+ln -sfr .local/bin/* $HOME/.local/bin/
