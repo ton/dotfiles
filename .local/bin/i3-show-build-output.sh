10f03e18c764dd1e31fd04320182715266621aca
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2018-06-22 00:00:14 +0200

    Fix Neovim build output using Alacritty

--- .local/bin/i3-show-build-output.sh
+++ .local/bin/i3-show-build-output.sh
@@ -2,6 +2,6 @@
 
 # Close all other build output windows that might still linger in the
 # scratchpad.
-i3-msg '[class="build_output"] kill' > /dev/null
+i3-msg '[instance="build_output"] kill' > /dev/null
 
-exec i3-sensible-terminal -c build_output -e tmux -f $HOME/.tmux.make.conf -L make new-session "trap 'rm $1' SIGINT ; tail -f $1"
+exec alacritty --class build_output -d 0 0 -e tmux -f $HOME/.tmux.make.conf -L make new-session "trap 'rm $1' SIGINT ; tail -f $1"
