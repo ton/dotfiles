7123a76511e14e7972ec14208ac55250ccb97c1c
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2020-05-13 11:59:15 +0200

    Simplify and fix switch-to-monitor.sh

--- scripts/.local/bin/switch-to-monitor.sh
+++ scripts/.local/bin/switch-to-monitor.sh
@@ -1,52 +1,22 @@
 #!/bin/sh
 
-MONITORS=()
-MONITORS+=("DP-2-2")
-MONITORS+=("DP-2")
-MONITORS+=("HDMI-1")
-MONITORS+=("DP2-2")
-MONITORS+=("DP2")
-MONITORS+=("HDMI1")
-
-LAPTOP_DISPLAYS=()
-LAPTOP_DISPLAYS+=("eDP-1")
-LAPTOP_DISPLAYS+=("eDP1")
-
 # Determine connected laptop display.
-for laptop_display in ${LAPTOP_DISPLAYS[@]}; do
-    STATUS=`xrandr -d ${DISPLAY} -q | grep ${laptop_display} | awk '{print $2}'`
-    if [ "${STATUS}" = "connected" ]; then
-        LAPTOP_DISPLAY=$laptop_display
-        break
-    fi
-done
+laptop_display=$(xrandr --listactivemonitors | grep ": +\*" | sed 's/.*\*\(\S*\).*/\1/')
 
-for monitor in ${MONITORS[@]}; do
-    STATUS=`xrandr -d ${DISPLAY} -q | grep ${monitor} | awk '{print $2}'`
-    echo $STATUS
+echo "Laptop display: $laptop_display"
 
-    if [ "${STATUS}" = "connected" ]; then
-        xrandr -d ${DISPLAY} --output ${monitor} --auto --output ${LAPTOP_DISPLAY} --off
+# Determine the connected monitor.
+xrandr --listactivemonitors | while read -r line; do
+    if echo "$line" | grep -qe ": +[^*]"; then
+        monitor=$(echo "$line" | sed 's/[^+]*+\(\S*\).*$/\1/')
+        xrandr -d ${DISPLAY} --output ${monitor} --auto --output ${laptop_display} --off
+        logger "Monitor connected, using monitor as primary output."
 
         # Rescale background image.
         sh $HOME/.fehbg &
 
-        # Export monitor environment variable (used by polybar).
-        export MONITOR=${monitor}
-
-        logger "Monitor connected, using monitor as primary output."
-
-        break
+        exit 0
     fi
 done
 
-if [ -z $MONITOR ]
-then
-    logger "Monitor not connected, using laptop display as primary output."
-
-    # Export monitor environment variable (used by polybar).
-    export MONITOR=${LAPTOP_DISPLAY}
-fi
-
-# Rescale background image.
-sh $HOME/.fehbg &
+exit 1
