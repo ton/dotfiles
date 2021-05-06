781e78bf740627bf6126c73335087fc0c62822a9
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2021-05-06 11:12:16 +0200

    nvim: fix interrupting build

--- scripts/.local/bin/i3-show-build-output.sh
+++ scripts/.local/bin/i3-show-build-output.sh
@@ -7,7 +7,8 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-build_cmd="$1"
+build_cmd_line="$1"
+build_cmd=$(echo "$build_cmd_line" | head -n 1 | cut -d " " -f1)
 build_log="$2"
 
 # Close all other build output windows that might still linger in the
@@ -21,9 +22,9 @@ touch build_log
 # Start a tmux session in a new terminal that shows the tail of the build
 # output window, for at most an hour (to be able to bring the build output back
 # after the build finishes).
-i3-sensible-terminal -c build_output -e tmux -f ~/.tmux.make.conf -L make new-session "tail -f $build_log && sleep 3600" &
+i3-sensible-terminal -c build_output -e tmux -f ~/.tmux.make.conf -L make new-session "trap 'killall $build_cmd' INT; tail -f $build_log && sleep 3600" &
 
 # Start the build; append output to the build log that indicates the result of
 # the build. Then, automatically hide the scratchpad output window.
-($build_cmd && echo -e '\nBuild completed successfully 😊' || echo -e '\nBuild failed 😢') 2>&1 | tee $build_log \
+($build_cmd_line && echo -e '\nBuild completed successfully 😊' || echo -e '\nBuild failed 😢') 2>&1 | tee "$build_log" \
     && i3-msg '[class="build_output"]' scratchpad show
