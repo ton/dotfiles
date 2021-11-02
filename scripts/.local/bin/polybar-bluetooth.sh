996cfa4dce2453b7936a72f722cf5f4ab0171757
Ton van den Heuvel <tonvandenheuvel@gmail.com>
2021-11-02 23:24:09 +0100

    polybar: use material design bluetooth icon

--- scripts/.local/bin/polybar-bluetooth.sh
+++ scripts/.local/bin/polybar-bluetooth.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+bluetooth_icon=$(printf '\Uf00af') # mdi-bluetooth (0xf00af)
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#66ffffff}"
+  echo "%{F#66ffffff}$bluetooth_icon"
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then
-    echo ""
+    echo "$bluetooth_icon"
   fi
-  echo "%{F#2193ff}"
+  echo "%{F#2193ff}$bluetooth_icon"
 fi
