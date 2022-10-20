c9a4c1567e2a790eddd38f0b0b4b893bb23ba042
Ton van den Heuvel <git@tonvandenheuvel.com>
2022-10-20 15:58:44 +0200

    Remove Fractal reference

--- scripts/.local/bin/profile.sh
+++ scripts/.local/bin/profile.sh
@@ -5,12 +5,10 @@ exit_with_usage() {
 Usage: profile.sh [--pdf <path to PDF viewer] <command to execute>"
 
 Profile the given command, and shows the PDF profile result using the given PDF
-viewer. In case no PDF viewer is specified, \$FRACTAL_PDF_VIEWER is used.
+viewer.
 
-Note; Fractal code has to be built with profiling enabled as follows, from the
-src/ directory:
-
-  FRC_PROFILE=1 ./configure release
+Note; code has to be built with profiling enabled as follows, from the src/
+directory.
 USAGE
   exit 1
 }
@@ -27,8 +25,6 @@ if [ "$1" = "--pdf" ]; then
   shift 1
   pdf_viewer=$(command -v "$1")
   shift 1
-else
-  pdf_viewer=$FRACTAL_PDF_VIEWER
 fi
 
 # In case no PDF viewer is configured, try some default options.
@@ -41,7 +37,7 @@ if [ ! -x "$pdf_viewer" ]; then
     fi
   done
   if [ ! -x "$pdf_viewer" ]; then
-    echo "No valid pdf reader found, specify on the command line or set \$FRACTAL_PDF_VIEWER."
+    echo "No valid pdf reader found, specify one on the command line."
     exit 1
   fi
 fi
