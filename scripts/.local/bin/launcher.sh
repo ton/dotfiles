b06a5d033ffa9752afbe9e23c84340159d56e358
Ton van den Heuvel <git@tonvandenheuvel.com>
2023-11-14 16:34:54 +0100

    launcher.sh: add additional programs

--- scripts/.local/bin/launcher.sh
+++ scripts/.local/bin/launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-entries="firefox st calculator.sh poweroff"
+entries="firefox st calculator.sh poweroff gimp snipit"
 
 exec $(echo $entries | tr ' ' '\n' | FZF_DEFAULT_OPTS="--reverse --no-info --prompt='$ ' --margin=1,2 --height=4 --no-scrollbar" fzfmenu.sh -t '' -f "Source Code Pro:size=24:weight:medium")
