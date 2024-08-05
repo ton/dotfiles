046674186a19b85cc024d4fdbfa1bb21fe2639e2
Ton van den Heuvel <git@tonvandenheuvel.com>
2024-08-05 20:20:48 +0200

    fzfmenu.sh: add support for foot terminal

--- scripts/.local/bin/fzfmenu.sh
+++ scripts/.local/bin/fzfmenu.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-st -c 'Floating' -g 40x4 "$@" sh -c "fzf < /proc/$$/fd/0 > /proc/$$/fd/1"
+if command -v st > /dev/null ; then
+    st -c 'Floating' -g 40x4 "$@" sh -c "fzf < /proc/$$/fd/0 > /proc/$$/fd/1"
+elif command -v foot > /dev/null ; then
+    foot -a 'Floating' -W 41x4 "$@" sh -c "fzf < /proc/$$/fd/0 > /proc/$$/fd/1"
+fi
--- scripts/.local/bin/launcher.sh
+++ scripts/.local/bin/launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 entries="firefox st calculator.sh poweroff reboot gimp snipit idea.sh blender freecad zeal"
 
-exec $(echo $entries | tr ' ' '\n' | FZF_DEFAULT_OPTS="--reverse --no-info --prompt='$ ' --margin=1,2 --height=4 --no-scrollbar" fzfmenu.sh -t '' -f "Source Code Pro:size=24:weight:medium")
+exec $(echo $entries | tr ' ' '\n' | FZF_DEFAULT_OPTS="--reverse --no-info --prompt='$ ' --margin=0,0 --padding=0,0,0,1 --height=4 --no-scrollbar" fzfmenu.sh -t '' -f "Source Code Pro:size=24:weight:medium")
