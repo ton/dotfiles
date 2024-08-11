67013622376e9327ba09b37d4e15d1241fd4bbb2
Ton van den Heuvel <git@tonvandenheuvel.com>
2024-08-11 14:39:09 +0200

    launcher.sh: add more commands

--- scripts/.local/bin/launcher.sh
+++ scripts/.local/bin/launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-entries="firefox st calculator.sh poweroff reboot gimp snipit idea.sh blender freecad zeal"
+entries="firefox st calculator.sh poweroff reboot gimp snipit idea.sh blender freecad zeal inkscape transmission-gtk passmenu"
 
 exec $(echo $entries | tr ' ' '\n' | FZF_DEFAULT_OPTS="--reverse --no-info --prompt='$ ' --margin=0,0 --padding=0,0,0,1 --height=4 --no-scrollbar" fzfmenu.sh -t '' -f "Source Code Pro:size=24:weight:medium")
