1da1beceee9e60f0b41890066197104555bcaf6c
Ton van den Heuvel <git@tonvandenheuvel.com>
2024-10-22 12:45:28 +0200

    launcher.sh: filter all executable commands

--- scripts/.local/bin/launcher.sh
+++ scripts/.local/bin/launcher.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-entries="firefox st calculator.sh poweroff reboot gimp snipit idea.sh blender freecad zeal inkscape transmission-gtk passmenu"
+#!/usr/bin/env zsh
+entries=$(whence -wm '*' | rg ': command' | sed 's/:[^:]*$//')
 
 exec $(echo $entries | tr ' ' '\n' | FZF_DEFAULT_OPTS="--reverse --no-info --prompt='$ ' --margin=0,0 --padding=0,0,0,1 --height=4 --no-scrollbar" fzfmenu.sh -t '' -f "Source Code Pro:size=24:weight:medium")
