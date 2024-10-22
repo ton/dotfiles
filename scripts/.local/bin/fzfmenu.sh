26ece224ac16a48eca2403020ec9633122bca852
Ton van den Heuvel <git@tonvandenheuvel.com>
2024-10-22 12:45:59 +0200

    fzfmenu.sh: use height of 6 lines

--- scripts/.local/bin/fzfmenu.sh
+++ scripts/.local/bin/fzfmenu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if command -v st > /dev/null ; then
-    st -c 'Floating' -g 40x4 "$@" sh -c "fzf < /proc/$$/fd/0 > /proc/$$/fd/1"
+    st -c 'Floating' -g 40x6 "$@" sh -c "fzf < /proc/$$/fd/0 > /proc/$$/fd/1"
 elif command -v foot > /dev/null ; then
-    foot -a 'Floating' -W 41x4 "$@" sh -c "fzf < /proc/$$/fd/0 > /proc/$$/fd/1"
+    foot -a 'Floating' -W 41x6 "$@" sh -c "fzf < /proc/$$/fd/0 > /proc/$$/fd/1"
 fi
--- scripts/.local/bin/launcher.sh
+++ scripts/.local/bin/launcher.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 entries=$(whence -wm '*' | rg ': command' | sed 's/:[^:]*$//')
 
-exec $(echo $entries | tr ' ' '\n' | FZF_DEFAULT_OPTS="--reverse --no-info --prompt='$ ' --margin=0,0 --padding=0,0,0,1 --height=4 --no-scrollbar" fzfmenu.sh -t '' -f "Source Code Pro:size=24:weight:medium")
+exec $(echo $entries | FZF_DEFAULT_OPTS="--reverse --no-separator --no-info --prompt='$ ' --margin=1,2 --height=6 --no-scrollbar" fzfmenu.sh -t '' -f "Source Code Pro:size=24:weight:medium")
