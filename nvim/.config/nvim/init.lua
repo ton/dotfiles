eb50a4fa700e8a489c264b214d84a16be4a944c8
Ton van den Heuvel <git@tonvandenheuvel.com>
2024-10-28 12:29:15 +0100

    Neovim: fix `cfile` prompting for enter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -222,7 +222,7 @@ function async_make()
     function(code, signal)
       vim.schedule(function()
         if vim.fn.filereadable(build_log) then
-          vim.cmd('cfile ' .. build_log)
+          vim.cmd('silent cfile ' .. build_log)
           vim.cmd('cw')
         end
       end)
